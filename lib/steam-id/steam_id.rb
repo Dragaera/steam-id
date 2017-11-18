fab941bf2926667c9f8545a28cb00f7f4a6683d5
Michael Senn <michael@senn.email>
2017-11-18 10:48:03 +0100

    Extract conversion between SteamIDs into separate class.

--- lib/steam-id/steam_id.rb
+++ lib/steam-id/steam_id.rb
@@ -1,119 +1,33 @@
-require 'steam-condenser'
+# coding: utf-8
 
 module SteamID
-  # Module to convert various formats of Steam IDs into an account ID.
-  module SteamID
-    STEAM_ID_64_OFFSET = 61197960265728
+  class SteamID
+    ID_64_OFFSET = 61197960265728
+    PROFILE_BASE_URL = "https://steamcommunity.com/profiles"
 
-    PATTERN_STEAM_ID = /^STEAM_[0-9]:([0-9]):([0-9]+)$/i
-    PATTERN_STEAM_ID_3 = /^\[?U:([0-9]{1,2}):([0-9]+)\]?$/i
-    # Must be at least 14 numbers, else ID_64 - OFFSET would be < 0.
-    PATTERN_STEAM_ID_64 = /^765([0-9]{11,})$/
-    # Not sure what its valid range is - exists with at least 7 and 8 numbers.
-    PATTERN_ACCOUNT_ID = /^[0-9]+$/
+    attr_reader :account_id
 
-    PATTERN_COMMUNITY_URL = /^https?:\/\/steamcommunity\.com\/profiles\/(765[0-9]+)\/?$/
-    PATTERN_COMMUNITY_URL_STEAM_ID_3 = /^https?:\/\/steamcommunity\.com\/profiles\/\[U:([0-9]{1,2}):([0-9]+)\]$/i
-    PATTERN_CUSTOM_URL = /^https?:\/\/steamcommunity\.com\/id\/([^\/]+)\/?$/
-
-    # Resolve custom URL into account ID suitable for API calls.
-    # @param s [String] custom URL: 
-    #   - http://steamcommunity.com/id/some-custom-url
-    #   - some-custom-url
-    # @return [Fixnum] Account ID
-    # @raise [ArgumentError] If the supplied string was not a valid community URL.
-    def self.from_vanity_url(url, steam_api_key:)
-      WebApi.api_key = steam_api_key
-
-      PATTERN_CUSTOM_URL.match(url) do |m|
-        url = m[1]
-      end
-
-      steam_id = SteamId.resolve_vanity_url(url)
-      if steam_id.nil?
-        raise ArgumentError, "#{ url } was not a valid custom URL."
-      else
-        from_steam_id(steam_id)
-      end
+    def initialize(account_id)
+      @account_id = account_id
     end
 
-    # Convert community URL into account ID suitable for API calls.
-    # @param s [String] community URL: http://steamcommunity.com/profiles/76561198008487038
-    # @return [Fixnum] Account ID
-    # @raise [ArgumentError] If the supplied string was not a valid community URL.
-    def self.from_community_url(url)
-      PATTERN_COMMUNITY_URL.match(url) do |m|
-        return self.from_steam_id(m[1])
-      end
+    def id
+      offset = @account_id / 2
+      index  = @account_id - 2 * offset
 
-      PATTERN_COMMUNITY_URL_STEAM_ID_3.match(url) do |m|
-        return self.from_steam_id(m[2])
-      end
-
-      raise ArgumentError, "#{ url.inspect } is not a supported community URL."
+      "STEAM_0:#{ index }:#{ offset }"
     end
 
-    # Convert Steam ID into account ID suitable for API calls.
-    # @param s [String] Steam ID, either of:
-    #   - Steam ID: STEAM_0:0:24110655
-    #   - Steam ID 3: U:1:48221310
-    #   - Steam ID 64: 76561198008487038
-    #   - Account ID: 48221310
-    # @return [Fixnum] Account ID
-    # @raise [ArgumentError] If the supplied string could not be converted to
-    #   an account ID.
-    def self.from_steam_id(id)
-      # In case we get a fixnum.
-      id = id.to_s
-
-      # https://developer.valvesoftware.com/wiki/SteamID#Format
-      PATTERN_STEAM_ID.match(id) do |m|
-        return m[1].to_i + m[2].to_i * 2
-      end
-
-      PATTERN_STEAM_ID_3.match(id) do |m|
-        return m[2].to_i
-      end
-
-      PATTERN_STEAM_ID_64.match(id) do |m|
-        return m[1].to_i - STEAM_ID_64_OFFSET
-      end
-
-      # Matching this one last, as not to catch an ID 64 on accident.
-      PATTERN_ACCOUNT_ID.match(id) do |m|
-        return id.to_i
-      end
-
-      # If we get until here, we did not match any regex.
-      raise ArgumentError, "#{ id.inspect } is not a supported SteamID."
+    def id_64
+      "765#{ @account_id + ID_64_OFFSET }".to_i
     end
 
-    def self.from_string(s, steam_api_key: nil)
-      account_id = nil
+    def id_3
+      "[U:1:#{ @account_id }]"
+    end
 
-      # Todo: Refactor
-      begin
-        # Checking for Steam ID first. Most restrictive check, and also does
-        # not require a call to Steam Web API.
-        account_id = from_steam_id(s)
-      rescue ArgumentError
-        begin
-          # Community URL afterwards, does not require an API call either.
-          account_id = from_community_url(s)
-        rescue ArgumentError
-          begin
-            # Trying to resolve as custom/vanity URL now.
-            account_id = from_vanity_url(s, steam_api_key: steam_api_key)
-          rescue ArgumentError
-          end
-        end
-      end
-
-      if account_id.nil?
-        raise ArgumentError, "Could not convert #{ s } to account id."
-      else
-        account_id
-      end
+    def profile_url
+      "#{ PROFILE_BASE_URL }/#{ id_64 }"
     end
   end
 end
--- spec/steam-id/steam_id_spec.rb
+++ spec/steam-id/steam_id_spec.rb
@@ -1,102 +1,34 @@
 module SteamID
   RSpec.describe SteamID do
-    STEAM_ID         = 'STEAM_0:0:24110655'
-    STEAM_ID_MIXED   = 'SteAM_0:0:24110655'
-    STEAM_ID_SHORT   = 'STEAM_1:0:2691362'
+    let(:steam_id) { SteamID.new(48221310) }
 
-    STEAM_ID_3          = 'U:1:48221310'
-    STEAM_ID_3_LOWER    = 'u:1:48221310'
-    STEAM_ID_3_SHORT    = 'U:1:4584616'
-    STEAM_ID_3_BRACKETS = '[U:1:48221310]'
-
-    STEAM_ID_64      = '76561198008487038'
-
-    COMMUNITY_URL = 'http://steamcommunity.com/profiles/76561198008487038'
-    COMMUNITY_URL_STEAM_ID_3 = 'http://steamcommunity.com/profiles/[U:1:48221310]'
-
-    describe '::from_steam_id' do
-      it 'supports SteamID as input' do
-        steam_id = SteamID.from_steam_id(STEAM_ID)
-        expect(steam_id).to eq 48221310
-
-        steam_id = SteamID.from_steam_id(STEAM_ID_SHORT)
-        expect(steam_id).to eq 5382724
-      end
-
-      it 'supports SteamID 3 as input' do
-        steam_id = SteamID.from_steam_id(STEAM_ID_3)
-        expect(steam_id).to eq 48221310
-
-        steam_id = SteamID.from_steam_id(STEAM_ID_3_BRACKETS)
-        expect(steam_id).to eq 48221310
-
-        steam_id = SteamID.from_steam_id(STEAM_ID_3_SHORT)
-        expect(steam_id).to eq 4584616
-      end
-
-      it 'is not case-sensitive' do
-        steam_id = SteamID.from_steam_id(STEAM_ID_MIXED)
-        expect(steam_id).to eq 48221310
-
-        steam_id = SteamID.from_steam_id(STEAM_ID_3_LOWER)
-        expect(steam_id).to eq 48221310
-      end
-
-
-      it 'supports SteamID 64 as input' do
-        steam_id = SteamID.from_steam_id(STEAM_ID_64)
-        expect(steam_id).to eq 48221310
-      end
-
-      it 'supports an account ID as input' do
-        steam_id = SteamID.from_steam_id(48221310)
-        expect(steam_id).to eq 48221310
-
-        # Special case, starts with 765, like a Steam ID
-        steam_id = SteamID.from_steam_id(7654321)
-        expect(steam_id).to eq 7654321
-      end
-
-      it 'raises an exception upon invalid input' do
-        expect { SteamID.from_steam_id('foobar') }.to raise_error(ArgumentError)
+    describe '#account_id' do
+      it 'should return the account ID' do
+        expect(steam_id.account_id).to eq 48221310
       end
     end
 
-    describe '::from_community_url' do
-      it 'supports community URLs as input' do
-        steam_id = SteamID.from_community_url(COMMUNITY_URL)
-        expect(steam_id).to eq 48221310
-      end
-
-      it 'supports community URLs with SteamID3 as input' do
-        steam_id = SteamID.from_community_url(COMMUNITY_URL_STEAM_ID_3)
-        expect(steam_id).to eq 48221310
-      end
-
-      it 'raises an exception upon invalid input' do
-        expect { SteamID.from_community_url('http://google.com/profiles/foo') }.to raise_error(ArgumentError)
+    describe '#id' do
+      it 'should return the Steam ID' do
+        expect(steam_id.id).to eq 'STEAM_0:0:24110655'
       end
     end
 
-    describe '::from_vanity_url' do
-      it 'supports custom URLs as input' do
-          expect(::SteamId).to receive(:resolve_vanity_url) { 54321 }
-          expect(SteamID).to receive(:from_steam_id) { 12345 }
-
-          expect(SteamID.from_vanity_url('some-test', steam_api_key: nil)).to eq 12345
+    describe '#id_64' do
+      it 'should return the Steam ID 64' do
+        expect(steam_id.id_64).to eq 76561198008487038
       end
+    end
 
-      it 'supports full custom URLs as input' do
-          expect(::SteamId).to receive(:resolve_vanity_url) { 54321 }
-          expect(SteamID).to receive(:from_steam_id) { 12345 }
-
-          expect(SteamID.from_vanity_url('http://steamcommunity.com/id/some-test', steam_api_key: nil)).to eq 12345
+    describe '#id_3' do
+      it 'should return the Steam ID 3' do
+        expect(steam_id.id_3).to eq '[U:1:48221310]'
       end
+    end
 
-      it 'raises an exception if it could not resolve the ID' do
-          expect(::SteamId).to receive(:resolve_vanity_url) { nil }
-
-          expect{ SteamID.from_vanity_url('this-better-not-be-a-valid-url', steam_api_key: nil) }.to raise_error(ArgumentError)
+    describe '#profile_url' do
+      it 'should return the profile URL' do
+        expect(steam_id.profile_url).to eq 'https://steamcommunity.com/profiles/76561198008487038'
       end
     end
   end
