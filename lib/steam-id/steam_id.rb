a3878f075571dc5a6676f7936f6bbaab50632bd6
Michael Senn <michael@senn.email>
2017-11-18 15:51:39 +0100

    Improve documentation, add convenience method to SteamID module.

--- lib/steam-id/steam_id.rb
+++ lib/steam-id/steam_id.rb
@@ -1,16 +1,22 @@
 # coding: utf-8
 
 module SteamID
+  # Class representing a single Steam ID - that is, one logical account
   class SteamID
-    ID_64_OFFSET = 61197960265728
+    # Offset between ID 64 and account ID.
+    ID_64_OFFSET = 76561197960265728
+    # Base URL for Steam community profiles based on ID64 / ID3.
     PROFILE_BASE_URL = "https://steamcommunity.com/profiles"
 
     attr_reader :account_id
 
+    # Create new SteamID
+    # @param account_id [Integer] Numeric account ID.
     def initialize(account_id)
       @account_id = account_id
     end
 
+    # @return [String] Classical Steam ID (eg STEAM_0:....)
     def id
       offset = @account_id / 2
       index  = @account_id - 2 * offset
@@ -18,14 +24,17 @@ module SteamID
       "STEAM_0:#{ index }:#{ offset }"
     end
 
+    # @return [Integer] Steam ID 64 / Steam profile ID (eg 765...)
     def id_64
-      "765#{ @account_id + ID_64_OFFSET }".to_i
+      @account_id + ID_64_OFFSET
     end
 
+    # @return [String] Steam ID 3 (eg [U:1:...])
     def id_3
       "[U:1:#{ @account_id }]"
     end
 
+    # @return [String] URL to player's profile on Steam community.
     def profile_url
       "#{ PROFILE_BASE_URL }/#{ id_64 }"
     end
